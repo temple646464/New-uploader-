45d61c8bfdf605875e113ae50d15de6a5659590a
temple646464 <Temple646464@gmail.com>
2025-05-01 13:21:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1645,17 +1645,6 @@ async def process_appxwp(bot: Client, m: Message, user_id: int):
             if session:
                 await session.close()
             await CONNECTOR.close()
-
-import os
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello from Flask!"
-
-if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
